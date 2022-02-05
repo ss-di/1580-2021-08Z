6536e6851c43e5e2594cc877b42f3a37ab11a2d2
Alexander Lobachev <ss_di@mail.ru>
2022-02-05 09:48:48 +0300

    раздор задач
    тумба юмба
    замена

---
+++ 2022_02_05-kons/56-e-tumba.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void perebor(int k, string alf, string cur, int &cnt) { 
+    if (k == 0) { // k == cur.size()
+        cout << cur << '\n';
+        cnt += 1;
+    } else {
+        for (int i = 0; i < int(alf.size()); i += 1)
+            perebor(k-1, alf, cur + alf.at(i), cnt);  
+    }
+}
+
+int main() {
+    int k, cnt = 0;
+    string a;
+    cin >> a;
+    cin >> k;
+    perebor(k, a, "", cnt);
+    cout << cnt;
+    return 0;
+}
---
+++ 2022_02_05-kons/64-a-zam.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+string zam(string s, string f, string t){
+    unsigned long long p;
+
+    p = s.find(f);
+    while (p != string::npos){
+        s.replace(p, f.size(), t);
+        p = s.find(f, p + t.size());
+    }
+    return s;
+}
+
+int main() {
+    string s, f, t;
+    getline(cin, s);
+    getline(cin, f);
+    getline(cin, t);
+    cout << zam(s, f, t);
+	return 0;
+}
+
+/*
+abcbc
+abc
+aaa
+===
+aaabc
+*/
