8717d5837d2beb1524ea0c1fd605a724d6b4ad43
Alexander Lobachev <ss_di@mail.ru>
2023-04-18 13:55:45 +0300

    zero 1-2

--- 2023_04_11-Zero/z01/main.cpp
+++ 2023_04_11-Zero/z01/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+double calcMult(const vector<vector<double>> &a){
+//---
+double res = 1;
+int n = int(a.size());
+for(int i = 0; i < n; i += 1) {
+    res *= a[i][i];
+    if (n % 2 == 0 || i != n / 2)
+        res *= a[i][n-i-1];
+}
+return res;
+//---
+}
 
 int main()
 {
--- 2023_04_11-Zero/z02/main.cpp
+++ 2023_04_11-Zero/z02/main.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n, mn_m=-1, kol = 0, h, m;
+    cin >> n;
+    for (int i = 0; i < n; i += 1){
+        cin >> h >> m;
+        m += h*60;
+        if (m < mn_m || mn_m == -1){
+            mn_m = m;
+            kol = 1;
+        } else if (m == mn_m)
+            kol += 1;
+    }
+    cout << mn_m / 60 << " " << mn_m % 60 << '\n';
+    if (kol == 1)
+        cout << "мер";
+    else
+        cout << kol << " дю";
     return 0;
 }
